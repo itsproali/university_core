8ccbed51cc9868cf0eee6ace96064d24d2dc3cc8
Mohammad Ali <itsproali@gmail.com>
2023-09-16 21:33:51 +0600

    logger removed

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,23 @@
 import { Server } from "http";
 import app from "./app";
 import config from "./config";
-import logger from "./shared/logger";
 
 // Uncaught exception
 process.on("uncaughtException", (err: any) => {
-  logger.error("UNCAUGHT EXCEPTION! 💣 Shutting down...");
-  logger.error(err.message);
+  console.error("UNCAUGHT EXCEPTION! 💣 Shutting down...");
+  console.error(err.message);
   process.exit(1);
 });
 
 async function bootstrap() {
   const server: Server = app.listen(config.PORT, () => {
-    logger.info(`Server listening on port ${config.PORT}`);
+    console.info(`Server listening on port ${config.PORT}`);
   });
 
   // Unhandled promise rejection
   process.on("unhandledRejection", (err: any) => {
-    logger.error("UNHANDLED REJECTION! 💥 Shutting down...");
-    logger.error(err.message);
+    console.error("UNHANDLED REJECTION! 💥 Shutting down...");
+    console.error(err.message);
     if (server) {
       server.close(() => {
         process.exit(1);
@@ -31,7 +30,7 @@ async function bootstrap() {
 
   // SIGTERM
   process.on("SIGTERM", () => {
-    logger.info("SIGTERM RECEIVED 🚦 Shutting down gracefully");
+    console.info("SIGTERM RECEIVED 🚦 Shutting down gracefully");
     if (server) {
       server.close();
     }
